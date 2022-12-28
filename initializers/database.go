f8246936d47e98a31902a4fb613f1ee05f510242
Timotej Avsec <avsectimotej1@gmail.com>
2022-12-28 10:17:42 +0100

    Add deployment kubernetes file

--- initializers/database.go
+++ initializers/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 
 func ConnectToMysql() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("MYSQL_DATABASE"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("MYSQL_DATABASE"))
 
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
